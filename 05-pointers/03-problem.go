febfb193c3bb9d42ae95dc78369e5c5232ca32d1
Magesh Kuppan <tkmagesh77@gmail.com>
2025-05-05 14:37:07 +0530

    practice problem

---
+++ 05-pointers/03-problem.go
@@ -0,0 +1,25 @@
+/*
+Build an interactive calculator. When the program is executed
+
+Display the following menu
+1. Add
+2. Subtract
+3. Multiply
+4. Divide
+5. Exit
+Accept the user choice
+
+if the choice == 5
+	exit the application
+
+if the choice is >= 1 and <= 4
+	accept two operands (numbers)
+	perform the corresponding operation
+	print the result
+	DISPLAY THE MENU AGAIN
+
+else
+	print "invalid choice"
+	DISPLAY THE MENU AGAIN
+
+*/
