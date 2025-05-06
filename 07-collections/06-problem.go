f95d91df01556a52c9d6b9fe6fbef84cb2853749
Magesh Kuppan <tkmagesh77@gmail.com>
2025-05-06 11:39:07 +0530

    assignment - solution

--- 07-collections/06-problem.go
+++ 07-collections/06-problem.go
@@ -6,8 +6,30 @@ Use "strings.Split()" to get the words
 	ex output:
 	5 letter words occurs the most with 31 occurrances
 */
+
+/*
+TODO : Refactor the following into functions (SRP)
+*/
 package main
+
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	str := "Commodo Lorem enim dolore culpa minim ipsum ex excepteur in Commodo duis nulla ex laborum irure sunt incididunt Incididunt amet Lorem amet dolor sit consectetur culpa esse quis laborum pariatur laborum fugiat mollit Mollit voluptate aliquip Lorem incididunt mollit pariatur eu enim proident culpa esse laborum voluptate Nostrud aliqua magna ipsum qui duis euNisi pariatur sit do magna Lorem nostrud voluptate occaecat occaecat quis dolore irure Velit aliqua reprehenderit eu duis aliqua excepteur duis non enim Nostrud qui voluptate enim eiusmod dolor proident laboris nostrud commodo laborum aliquip sunt Exercitation do anim do ullamco Ipsum eiusmod aute qui ea consectetur Veniam laborum occaecat mollit pariatur commodo id ullamco dolore ipsum sit dolore elit fugiatUt ad aliqua dolor quis velit reprehenderit Dolore aliquip exercitation do fugiat Pariatur irure aliqua magna quis"
+
+	words := strings.Split(str, " ")
+	var wordsCountBySize = make(map[int]int)
+	for _, word := range words {
+		wordsCountBySize[len(word)]++
+	}
+	var maxCount, maxSize int
+	for size, count := range wordsCountBySize {
+		if count > maxCount {
+			maxSize, maxCount = size, count
+		}
+	}
+	fmt.Printf("%d letter occurs the most with %d occurrences\n", maxSize, maxCount)
 }
