cbec00ebf6746745dd74f0aed0452e03834bc8e8
Magesh Kuppan <tkmagesh77@gmail.com>
2025-05-06 15:03:34 +0530

    Packages

--- 10-modularity-demo/app.go
+++ 10-modularity-demo/app.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/tkmagesh/Cisco-GoFoundation-May-2025/10-modularity-demo/calculator"
+	"github.com/tkmagesh/Cisco-GoFoundation-May-2025/10-modularity-demo/calculator/utils"
+)
 
 func run() {
 	fmt.Println("Application executed")
+	fmt.Println(calculator.Add(100, 200))
+	fmt.Println(calculator.Subtract(100, 200))
+	fmt.Println("Op Count :", calculator.GetOpCount())
+
+	fmt.Println("Is 21 even ?:", utils.IsEven(21))
+	fmt.Println("Is 21 odd ?:", utils.IsOdd(21))
 }
--- 10-modularity-demo/go.mod
+++ 10-modularity-demo/go.mod
@@ -1,3 +1,3 @@
-module github.com/tkmagesh/Cisco-GoFoundation-May-2025/10-modulary-demo
+module github.com/tkmagesh/Cisco-GoFoundation-May-2025/10-modularity-demo
 
 go 1.23.3
