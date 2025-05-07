483d34daac6ecc547e7d625042bdbf623cc8c46f
Magesh Kuppan <tkmagesh77@gmail.com>
2025-05-07 11:19:04 +0530

    type assertion - assignment

---
+++ 13-type-assertion/02-problem.go
@@ -0,0 +1,18 @@
+/*
+Hint : Use strconv.Atoi() to convert integers in string format to integers
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(sum(10))               //=> 10
+	fmt.Println(sum(10, 20))           //=> 30
+	fmt.Println(sum(10, 20, "30", 40)) //=> 100
+	fmt.Println(sum("abc", 10))        //=> 10
+	fmt.Println(sum())                 //=> 0
+}
+
+func sum( /* ? */ ) /* ? */ {
+	/* ? */
+}
