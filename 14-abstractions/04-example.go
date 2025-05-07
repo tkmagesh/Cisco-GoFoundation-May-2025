0231edc07afd28f20e4a8bccd95268f83cfc5fd1
Magesh Kuppan <tkmagesh77@gmail.com>
2025-05-07 12:31:24 +0530

    abstraction assignment

---
+++ 14-abstractions/04-example.go
@@ -0,0 +1,79 @@
+/*
+Accommodate "PerishableProduct" in the below implementation
+*/
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+/* Product Type */
+type Product struct {
+	Id       int
+	Name     string
+	UnitCost float64
+}
+
+func (p Product) String() string {
+	return fmt.Sprintf("Id = %d, Name = %q, UnitCost = %0.2f", p.Id, p.Name, p.UnitCost)
+}
+
+type LineItem struct {
+	Prod  Product
+	Units int
+}
+
+func (li LineItem) ItemCost() float64 {
+	return float64(li.Units) * li.Prod.UnitCost
+}
+
+// fmt.Stringer interface implementation
+func (li LineItem) String() string {
+	return fmt.Sprintf("%s, Units = %d, Item Cost = %0.2f", li.Prod, li.Units, li.ItemCost())
+}
+
+type ShoppingCart struct {
+	LineItems []LineItem
+}
+
+func (sc *ShoppingCart) AddItem(prod Product, units int) {
+	lineItem := LineItem{Prod: prod, Units: units}
+	sc.LineItems = append(sc.LineItems, lineItem)
+}
+
+func (sc ShoppingCart) GetCartValue() float64 {
+	cartValue := float64(0)
+	for _, lineItem := range sc.LineItems {
+		cartValue += lineItem.ItemCost()
+	}
+	return cartValue
+}
+
+func (sc ShoppingCart) String() string {
+	var sb strings.Builder
+	for _, lineItem := range sc.LineItems {
+		sb.WriteString(fmt.Sprintf("%s\n", lineItem))
+	}
+	sb.WriteString("============================================================================================\n")
+	sb.WriteString(fmt.Sprintf("Cart Value : %0.2f\n", sc.GetCartValue()))
+	return sb.String()
+}
+
+func main() {
+	pen := Product{Id: 100, Name: "Pen", UnitCost: 10}
+	pencil := Product{Id: 101, Name: "Pencil", UnitCost: 5}
+	marker := Product{Id: 102, Name: "Marker", UnitCost: 50}
+
+	cart := ShoppingCart{}
+	cart.AddItem(pen, 10)
+	cart.AddItem(pencil, 30)
+	cart.AddItem(marker, 4)
+
+	// The following should be possible
+	milk := PerishableProduct{Id: 200, Name: "Milk", UnitCost: 40}
+	cart.AddItem(milk, 2)
+
+	fmt.Println(cart)
+}
