7e1358e04496fea64a9001b65f7b151aa6fac0b6
Magesh Kuppan <tkmagesh77@gmail.com>
2025-05-07 09:44:43 +0530

    updates

---
+++ 12-composition/02-problem.go
@@ -0,0 +1,14 @@
+/*
+Create the respective types to represent a Shopping Cart
+
+Product
+	Id, Name, UnitCost
+LineItem
+	Product, Units
+ShoppingCart
+	Multiple LineItems
+
+1. User should be able to add items to the cart ()
+2. Calculate the overall cart value and print
+
+*/
