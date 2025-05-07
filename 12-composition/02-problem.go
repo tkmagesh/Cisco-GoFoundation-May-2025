b4766f7d6eb62804220d2cf43e011230de2599c9
Magesh Kuppan <tkmagesh77@gmail.com>
2025-05-07 10:22:42 +0530

    OOP assignment - solution

--- 12-composition/02-problem.go
+++ 12-composition/02-problem.go
@@ -10,5 +10,73 @@ ShoppingCart
 
 1. User should be able to add items to the cart ()
 2. Calculate the overall cart value and print
-
 */
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Product struct {
+	Id       int
+	Name     string
+	UnitCost float64
+}
+
+func (p Product) Format() string {
+	return fmt.Sprintf("Id = %d, Name = %q, UnitCost = %0.2f", p.Id, p.Name, p.UnitCost)
+}
+
+type LineItem struct {
+	Prod  Product
+	Units int
+}
+
+func (li LineItem) ItemCost() float64 {
+	return float64(li.Units) * li.Prod.UnitCost
+}
+
+func (li LineItem) Format() string {
+	return fmt.Sprintf("%s, Units = %d, Item Cost = %0.2f", li.Prod.Format(), li.Units, li.ItemCost())
+}
+
+type ShoppingCart struct {
+	LineItems []LineItem
+}
+
+func (sc *ShoppingCart) AddItem(prod Product, units int) {
+	lineItem := LineItem{Prod: prod, Units: units}
+	sc.LineItems = append(sc.LineItems, lineItem)
+}
+
+func (sc ShoppingCart) GetCartValue() float64 {
+	cartValue := float64(0)
+	for _, lineItem := range sc.LineItems {
+		cartValue += lineItem.ItemCost()
+	}
+	return cartValue
+}
+
+func (sc ShoppingCart) Format() string {
+	var sb strings.Builder
+	for _, lineItem := range sc.LineItems {
+		sb.WriteString(fmt.Sprintf("%s\n", lineItem.Format()))
+	}
+	sb.WriteString("============================================================================================\n")
+	sb.WriteString(fmt.Sprintf("Cart Value : %0.2f\n", sc.GetCartValue()))
+	return sb.String()
+}
+
+func main() {
+	pen := Product{Id: 100, Name: "Pen", UnitCost: 10}
+	pencil := Product{Id: 101, Name: "Pencil", UnitCost: 5}
+	marker := Product{Id: 102, Name: "Marker", UnitCost: 50}
+
+	cart := ShoppingCart{}
+	cart.AddItem(pen, 10)
+	cart.AddItem(pencil, 30)
+	cart.AddItem(marker, 4)
+	fmt.Println(cart.Format())
+}
